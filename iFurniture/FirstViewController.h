5f46d4f1e50f7abc874c04f63c8fd15e395dbeeb
shanwenyu <shanwenyu@shanwenyus-Mac-mini.local>
2011-05-26 13:41:24 +0800

    Merge Grid

--- iFurniture/FirstViewController.h
+++ iFurniture/FirstViewController.h
@@ -7,10 +7,49 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "AQGridView.h"
+#import "ImageDemoCellChooser.h"
+#import "ScrollViewViewController.h"
 
 
-@interface FirstViewController : UIViewController {
+@interface FirstViewController : UIViewController<AQGridViewDelegate, AQGridViewDataSource, ImageDemoCellChooserDelegate> {
 
+    
+    NSArray * _orderedImageNames;
+    NSArray * _imageNames;
+    AQGridView * _gridView;
+    
+    NSUInteger _cellType;
+    UIPopoverController * _menuPopoverController;
+    
+    UIPopoverController *popoverController;
+    UIToolbar *toolbar;
+    
+    id detailItem;
+    UILabel *detailDescriptionLabel;
+    UIWebView *webView;
+	UIBarButtonItem *languageButton;
+    UIPopoverController *languagePopoverController;
+    NSString *languageString;	
+    
+    ScrollViewViewController *scrollView;
 }
+
+@property (nonatomic, retain) IBOutlet AQGridView * gridView;
+
+- (IBAction) shuffle;
+- (IBAction) resetOrder;
+- (IBAction) displayCellTypeMenu: (UIBarButtonItem *) sender;
+- (IBAction) toggleLayoutDirection: (UIBarButtonItem *) sender;
+
+@property (nonatomic, retain) IBOutlet UIToolbar *toolbar;
+
+//@property (nonatomic, retain) id detailItem;
+//@property (nonatomic, retain) IBOutlet UILabel *detailDescriptionLabel;
+//@property (nonatomic, retain) IBOutlet UIWebView *webView;
+//@property (nonatomic, retain) IBOutlet UIBarButtonItem *languageButton;
+//@property (nonatomic, retain) UIPopoverController *languagePopoverController;
+//@property (nonatomic, copy) NSString *languageString;
+- (IBAction)touchLanguageButton;
 
 @end
